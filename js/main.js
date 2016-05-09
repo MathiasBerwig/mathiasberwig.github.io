bebc2753972a0546ac36dd361e4e90771cc454e7
Mathias Berwig <mathiasberwig@gmail.com>
2016-05-08 22:33:03 -0300

    Background alterado para animação particles.js
    Imagem padrão do tema substituída pela animação canvas da biblioteca particles.js (https://github.com/VincentGarreau/particles.js/).
    Pequenas modificações no CSS e HTML.

--- js/main.js
+++ js/main.js
@@ -2,16 +2,19 @@
 layout: null
 ---
 $(document).ready(function () {
-  $('a.blog-button').click(function (e) {
+  $('a.blog-button').click(function (e) {    
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
+      $('.content-wrapper').addClass('animated slideInRight')      
     } else {
       $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
-    }
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {
+        // Refresh the particles.js container
+        window.pJSDom[0].pJS.fn.particlesRefresh()
+      })      
+    }    
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
