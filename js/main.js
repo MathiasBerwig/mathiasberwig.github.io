1ece00b60ac77bb2dd09d7d1096d00926f6c2187
Mathias Berwig <mathiasberwig@gmail.com>
2016-07-22 14:37:47 -0300

    Suporte básico à multi idiomas adicionado
    Ainda precisa de alguns ajustes, mas já é um grande avanço perto do que
    tínhamos antes.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,50 @@
 ---
 layout: null
 ---
-$(document).ready(function () {
-  $('a.blog-button').click(function (e) {    
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
-    currentWidth = $('.panel-cover').width();
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {        
-        updateAnimation();
-      })      
-    }    
-  })
+$(document).ready(function() {
+    $('a.blog-button').click(function(e) {
+      if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+      currentWidth = $('.panel-cover').width();
+      if (currentWidth < 960) {
+          $('.panel-cover').addClass('panel-cover--collapsed');
+          $('.content-wrapper').addClass('animated slideInRight');
+      } else {
+          $('.panel-cover').css('max-width', currentWidth);
+          $('.panel-cover').animate({
+              'max-width': '530px',
+              'width': '40%'
+          }, 400, swing = 'swing', function() {
+              updateAnimation();
+          })
+      }
+  });
 
+  // Colapsa o painel principal quando na url /#blog 
   if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-    updateAnimation();
+      $('.panel-cover').addClass('panel-cover--collapsed');
+      updateAnimation();
   }
 
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-    updateAnimation();
+  // Colapsa o painel principal na url / (ou suas variantes em outros idiomas)
+  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}' + site_active_lang + '/') {
+      $('.panel-cover').addClass('panel-cover--collapsed');
+      updateAnimation();
   }
 
-  $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
+  // OnClick do botão de menu mobile
+  $('.btn-mobile-menu').click(function() {
+      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+      $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+  });
 
-  $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
-
+  // OnClick do botão
+  $('.navigation-wrapper .blog-button').click(function() {
+      $('.navigation-wrapper').toggleClass('visible');
+      $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+  });  
 });
 
-function updateAnimation(){
-  // Refresh the particles.js container
-  window.pJSDom[0].pJS.fn.particlesRefresh();
+function updateAnimation() {
+    // Refresh the particles.js container
+    window.pJSDom[0].pJS.fn.particlesRefresh();
 }
