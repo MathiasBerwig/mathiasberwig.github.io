3e70a8f4b8730174ce5a04c1c7bb3be07b812479
Mathias Berwig <mathiasberwig@gmail.com>
2016-07-27 15:44:27 -0300

    #blog movido para /blog/
    Agora a página inicial mostrará apenas aquela animação bonita.

--- js/main.js
+++ js/main.js
@@ -2,30 +2,7 @@
 layout: null
 ---
 $(document).ready(function() {
-    $('a.blog-button').click(function(e) {
-      if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
-      currentWidth = $('.panel-cover').width();
-      if (currentWidth < 960) {
-          $('.panel-cover').addClass('panel-cover--collapsed');
-          $('.content-wrapper').addClass('animated slideInRight');
-      } else {
-          $('.panel-cover').css('max-width', currentWidth);
-          $('.panel-cover').animate({
-              'max-width': '530px',
-              'width': '40%'
-          }, 400, swing = 'swing', function() {
-              updateAnimation();
-          })
-      }
-  });
-
-  // Colapsa o painel principal quando na url /#blog 
-  if (window.location.hash && window.location.hash == '#blog') {
-      $('.panel-cover').addClass('panel-cover--collapsed');
-      updateAnimation();
-  }
-
-  // Colapsa o painel principal na url / (ou suas variantes em outros idiomas)
+  // Colapsa o painel principal quando não estiver em / (ou suas variantes em outros idiomas)
   if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}' + site_active_lang + '/') {
       $('.panel-cover').addClass('panel-cover--collapsed');
       updateAnimation();
