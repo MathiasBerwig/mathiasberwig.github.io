6895846f8e89bbf693f8b70f0981536325a849e8
Mathias Berwig <mathiasberwig@gmail.com>
2016-07-28 13:57:47 -0300

    Comentários reescritos em inglês
    Bora deixar isso aí padronizado né.

--- js/main.js
+++ js/main.js
@@ -4,23 +4,23 @@ layout: null
 $(document).ready(function() {
   updateFooterPos();
 
-  // Colapsa o painel principal quando não estiver em / (ou suas variantes em outros idiomas)
+  // Collapse the panel when not in / (or its language variations)
   if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}' + site_active_lang + '/') {
       $('.panel-cover').addClass('panel-cover--collapsed');
       updateAnimation();
   }
 
-  // OnClick do botão de menu mobile
+  // OnClick for btn open navigation on mobile
   $('.btn-mobile-menu').click(function() {
       $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
       $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
   });
 
-  // OnClick do botão
+  // OnClick for btn close navigation on mobile
   $('.navigation-wrapper .blog-button').click(function() {
       $('.navigation-wrapper').toggleClass('visible');
       $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
-  });  
+  });
 
   // Window resize
   $( window ).resize(function() {
@@ -29,8 +29,8 @@ $(document).ready(function() {
 });
 
 function updateAnimation() {
-    // Refresh the particles.js container
-    window.pJSDom[0].pJS.fn.particlesRefresh();
+  // Refresh the particles.js container
+  window.pJSDom[0].pJS.fn.particlesRefresh();
 }
 
 function updateFooterPos() {
