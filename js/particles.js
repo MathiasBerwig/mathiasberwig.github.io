a7a35e7c84ff89488089427732a610424857f555
Mathias Berwig <mathiasberwig@gmail.com>
2016-08-10 15:18:12 -0300

    Primeiro build

---
+++ js/particles.js
@@ -0,0 +1,110 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 110,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#51aad9"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#ffffff"
+      },
+      "polygon": {
+        "nb_sides": 7
+      },
+      "image": {
+        "src": "",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 4,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 4,
+        "size_min": 0.3,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#9cd9f9",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 600
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 250,
+        "size": 0,
+        "duration": 2.5,
+        "opacity": 0,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 120,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
---
+++ pt/js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {  
+  // Collapse the panel when not in / (or its language variations)
+  if (!isBaseUrl()) {
+      $('.panel-cover').addClass('panel-cover--collapsed');
+      updateAnimation();
+  }
+
+  // OnClick for btn open navigation on mobile
+  $('.btn-mobile-menu').click(function() {
+      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+      $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+  });
+
+  // OnClick for btn close navigation on mobile
+  $('.navigation-wrapper .blog-button').click(function() {
+      $('.navigation-wrapper').toggleClass('visible');
+      $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+  });
+
+  // Window resize
+  $( window ).resize(function() {
+    windowResize();
+  });
+
+  windowResize();
+
+  // Initializes the headroom lib
+  $(".headroom").headroom({
+    "offset": 205,
+    "tolerance": 5,
+    "classes": {
+      "initial": "animated",
+      "pinned": "slideDown",
+      "unpinned": "slideUp"
+    }
+  });
+});
+
+function updateAnimation() {
+  // Refresh the particles.js container
+  window.pJSDom[0].pJS.fn.particlesRefresh();
+}
+
+function windowResize() {
+  // Check if the window does have a scroll bar then swap the footer position according to window size (relative or absolute)
+  document.body.scrollHeight > document.body.clientHeight ? $('.footer').css('position', 'relative') : $('.footer').css('position', 'absolute');
+
+  // Show/hide the language navigation items according to URL and body size
+  if (isBaseUrl() || window.matchMedia('(min-height: 530px)').matches && !$('.btn-mobile-menu').is(':visible')) {
+    $('.navigation--lang').show();
+  } else {
+    $('.navigation--lang').hide();
+  }
+}
+
+function isBaseUrl() {
+  return !(window.location.pathname !== '/' && window.location.pathname !== '/pt/');
+}
